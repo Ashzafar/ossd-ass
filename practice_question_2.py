47df6550b5063c3f0f8267a46fea9266aa36bc75
Muhammad Saad Naeem <f2020266454@umt.edu.pk>
2023-10-26 11:58:01 +0500

    practice Q2. Check Prime Number

---
+++ practice_question_2.py
@@ -0,0 +1,11 @@
+# f2020266454
+def is_prime(number):
+    divisor = number - 1
+    flag=True
+    while divisor > 1:
+        if number % divisor == 0:
+            flag=False
+        divisor = divisor - 1
+    print("is prime" if flag==True else "not prime")
+
+is_prime(4)
