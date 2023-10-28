11c11e57304429db7deaf85b6a8b00970c0195d1
Muhammad Saad Naeem <f2020266454@umt.edu.pk>
2023-10-28 13:38:10 +0500

    practice_question_6_reverse_string

---
+++ practice_question_6_reverse_string.py
@@ -0,0 +1,8 @@
+def reverse_string(string):
+    i=len(string)-1
+    reversed=[]
+    while i >=0:
+        reversed.append(string[i])
+        i-=1
+    print(''.join(reversed))
+reverse_string('join')
